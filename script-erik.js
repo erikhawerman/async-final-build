d5f34c80279ee9fb6315b59e4c94cdddf9c1857f
erikhawerman <84011896+erikhawerman@users.noreply.github.com>
2021-09-27 13:08:55 +0200

    mot slutet

--- script-erik.js
+++ script-erik.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
-  const btnMySkills = document.getElementById("vad-jag-kan");
-  const btnWhoAmI = document.getElementById("vem-är-jag");
-  const btnContactMe = document.getElementById("kontakta-mig");
+  const btnMySkills = document.getElementById("my-skills");
+  const btnWhoAmI = document.getElementById("who-am-i");
+  const btnContactMe = document.getElementById("contact-me");
   const secondPage = document.querySelector(".second-page");
   const thirdPage = document.querySelector(".third-page");
   const fourthPage = document.querySelector(".fourth-page");
-  const skills = document.querySelectorAll(".egenskap-container");
+  const attributes = document.querySelectorAll(".attribute-container");
 
+  // Gör att varje knapp lysnar efter att bli klickad på och skrollar då ner sidan till sin sida.
   btnMySkills.addEventListener("click", function () {
     secondPage.scrollIntoView({ behavior: "smooth" });
   });
@@ -18,19 +19,20 @@ $(document).ready(function () {
   });
 
   // Egenskaper callback - visar egenskaper och tar sedan bort observer för högre performance
-  const skillCallback = function (entries) {
+  const attributeCallback = function (entries) {
     entries.forEach(function (e) {
       if (!e.isIntersecting) return;
       e.target.classList.remove("hidden");
       observeSkills.unobserve(e.target);
     });
   };
-  const observeSkills = new IntersectionObserver(skillCallback, {
+  // Observer som letar att objekten som observeras och anropar callback-funktionen vid 15% korsning.
+  const observeAttributes = new IntersectionObserver(attributeCallback, {
     root: null,
     threshold: 0.15,
   });
-
-  skills.forEach(function (skill) {
-    observeSkills.observe(skill);
+  // Loopar igenom alla egenskaper och ber observern att observera samtliga element.
+  attributes.forEach(function (attribute) {
+    observeAttributes.observe(attribute);
   });
 });
--- script-robsan.js
+++ script-robsan.js
@@ -1,5 +1,5 @@
-const lådor = document.querySelectorAll(".kunskapsLåda");
-const bars = document.querySelectorAll(".bar");
+const lådor = document.querySelectorAll(".kunskapsLada");
+const barsRobin = document.querySelectorAll(".bar");
 const btnSkills = document.getElementById("skills");
 const btnMe = document.getElementById("hittaMig");
 const secondPage = document.getElementById("secondPage");
@@ -13,21 +13,35 @@ btnMe.addEventListener("click", function () {
 });
 // fyller kunskapsbaren i procent med hjälp av hoverfunctionen, och klick på mobile first
 const procentBar = function (bar, procent) {
-  bars[bar - 1].procent = procent;
+  barsRobin[bar - 1].procent = procent;
 };
-procentBar(1, 59);
-procentBar(2, 42);
-procentBar(3, 65);
-procentBar(4, 83);
-procentBar(5, 73);
+
+
+var kunskapsProcent= $(".stapel-procent").map(function() {
+  return $(this).text().replace(/[^\d]/g, "");  
+}).get();
+
+//funktion för att föra in rätt parametrar i procentStapel
+// function deklareraStaplar(){
+for(let i = 1; i < $(".stapel-procent").length + 1; i++){
+  procentBar(i, kunskapsProcent[i-1]);
+  console.log(( i, kunskapsProcent[i-1]));
+}
+
+// procentBar(1, 59);
+// procentBar(2, 42);
+// procentBar(3, 65);
+// procentBar(4, 83);
+// procentBar(5, 73);
 
 lådor.forEach(function (låda, i) {
   låda.addEventListener("mouseenter", function () {
     let index = 0;
     setInterval(function () {
-      if (index >= bars[i].procent) return;
+      if (index >= barsRobin[i].procent) return;
       index++;
-      bars[i].style.width = `${index}%`;
+      barsRobin[i].style.width = `${index}%`;
     }, 10);
   });
 });
+
